c8d9df6796fff933b24b2985659a8b30974503c1
Cléber Severo <clebersevero1998@gmail.com>
2022-11-25 19:01:43 -0300

    First files

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+function Banner () {
+return (
+    <header className='banner'>
+        <img src="/imagens/Banner.png" alt="Banner principal da pagina do organo"/>
+    </header>
+)
+}
+
+export default Banner
