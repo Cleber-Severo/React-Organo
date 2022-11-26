9481b65f1143ef15966e7622f8804725395a845b
Cléber Severo <clebersevero1998@gmail.com>
2022-11-26 14:27:37 -0300

    Adding the select and using map arrays

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,5 +1,16 @@
 import CampoTexto from '../CampoTexto';
+import ListaSuspensa from '../ListaSuspensa';
 import './Formulario.css'
+
+const times = [
+  'Programação',
+  'Front-End',
+  'Data Science',
+  'Devops',
+  'UX e Design',
+  'Mobile',
+  'Inovação e Gestão'
+]
 
 const Formulario = () => {
     return(
@@ -10,6 +21,7 @@ const Formulario = () => {
                 <CampoTexto label="Nome" placeholder="Digite seu nome"/>
                 <CampoTexto label="cargo" placeholder="Digite seu cargo"/>
                 <CampoTexto label="imagem" placeholder="Informe o endereço da imagem"/>
+                <ListaSuspensa label="Time" itens={times}/>
             </form>
         </section>
 
