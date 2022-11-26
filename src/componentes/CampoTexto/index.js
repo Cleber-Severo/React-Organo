50f397798eb6e4cae8f12d730db536ed129a1691
Cléber Severo <clebersevero1998@gmail.com>
2022-11-26 15:42:19 -0300

    input onchange method test

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,12 +1,16 @@
 import './CampoTexto.css'
 
+const aoDigitar = (evento) => {
+    console.log(evento.target.value)
+}
+
 const CampoTexto = (props) => {
     const alteraPlaceholder = `${props.placeholder}...`
 
     return (
     <div className='campo-texto'>
         <label>{props.label}</label>
-        <input required={props.obrigatorio} placeholder= {alteraPlaceholder} />
+        <input onChange={aoDigitar} required={props.obrigatorio} placeholder= {alteraPlaceholder} />
     </div>
     )
 }
