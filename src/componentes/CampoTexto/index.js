a0016783a6626e807a7e45c01845fdde37c6ece5
Cléber Severo <clebersevero1998@gmail.com>
2022-11-26 15:13:57 -0300

    Form validation by props

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -6,7 +6,7 @@ const CampoTexto = (props) => {
     return (
     <div className='campo-texto'>
         <label>{props.label}</label>
-        <input placeholder= {alteraPlaceholder} />
+        <input required={props.obrigatorio} placeholder= {alteraPlaceholder} />
     </div>
     )
 }
--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -23,10 +23,10 @@ const Formulario = () => {
         <section className="formulario">
             <form onSubmit={confirmaFormulario}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <CampoTexto label="Nome" placeholder="Digite seu nome"/>
-                <CampoTexto label="cargo" placeholder="Digite seu cargo"/>
+                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome"/>
+                <CampoTexto obrigatorio={true} label="cargo" placeholder="Digite seu cargo"/>
                 <CampoTexto label="imagem" placeholder="Informe o endereço da imagem"/>
-                <ListaSuspensa label="Time" itens={times}/>
+                <ListaSuspensa obrigatorio={true} label="Time" itens={times}/>
                 <Botao>
                     Criar card
                 </Botao>
