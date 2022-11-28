0bb039c637c53d650ff0f07b313af6bd8410f340
Cléber Severo <clebersevero1998@gmail.com>
2022-11-27 23:52:26 -0300

    setting JSX conditional to only render the teams that are different from null

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -6,15 +6,17 @@ const Time = (props) => {
     const css = {backgroundColor: props.corSecundaria}
 
     return(
-        <section className='time' style={css}>
+
+       props.colaboradores.length > 0 && <section className='time' style={css}>
             <h3 style={ {borderColor: props.corPrimaria} } >{props.nome}</h3>
             <div className='colaboradores'>
-                
+
                 {props.colaboradores.map(colaborador => <Colaborador nome={colaborador.nome} imagem={colaborador.imagem} cargo={colaborador.cargo} /> )}
 
             </div>
 
         </section>
+        
     )
 }
 
