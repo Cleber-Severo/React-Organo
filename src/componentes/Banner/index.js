b0b2a27550785383cd549064e7d304670ed29aca
Cléber Severo <clebersevero1998@gmail.com>
2022-11-28 17:12:36 -0300

    img

--- src/componentes/Banner/index.js
+++ src/componentes/Banner/index.js
@@ -3,7 +3,7 @@ import './Banner.css'
 const Banner = () => {
 return (
     <header className='banner'>
-        <img src="/imagens/Banner.png" alt="Banner principal da pagina do organo"/>
+        <img src="/imagens/banner.png" alt="Banner principal da pagina do organo"/>
     </header>
 )
 }
