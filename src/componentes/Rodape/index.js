eaec90919e7ba82e8a735964c5e904821b4d93f3
Cléber Severo <clebersevero1998@gmail.com>
2022-11-28 17:20:07 -0300

    Footer component

---
+++ src/componentes/Rodape/index.js
@@ -0,0 +1,13 @@
+import './Rodape.css'
+
+const Rodape = () => {
+
+    return(
+        <footer className='rodape'>
+            <img src="/imagens/Rodape.png"/>
+        </footer>
+    )
+
+}
+
+export default Rodape
