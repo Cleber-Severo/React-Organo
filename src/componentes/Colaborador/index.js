f9060a2162be2faf7f8f6f400c91c4f66653999b
Cléber Severo <clebersevero1998@gmail.com>
2022-11-27 22:58:50 -0300

    Creating the team cards component and styling it

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,20 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+
+    return(
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src='https://github.com/Cleber-Severo.png' alt='' />
+            </div>
+
+            <div className='rodape'>
+                <h4>Cléber</h4>
+                <h5>Desenvolvedor</h5>
+            </div>
+        </div>
+    )
+
+}
+
+export default Colaborador 
