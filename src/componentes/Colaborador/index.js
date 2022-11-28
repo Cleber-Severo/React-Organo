00cd52fee832e261a818c213d8374b0c74ebd627
Cléber Severo <clebersevero1998@gmail.com>
2022-11-27 23:33:11 -0300

    Inserting card via the form info with the arrat info by map method

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,16 +1,16 @@
 import './Colaborador.css'
 
-const Colaborador = () => {
+const Colaborador = ( {nome, imagem, cargo} ) => {
 
     return(
         <div className='colaborador'>
             <div className='cabecalho'>
-                <img src='https://github.com/Cleber-Severo.png' alt='' />
+                <img src={imagem} alt={nome} />
             </div>
 
             <div className='rodape'>
-                <h4>Cléber</h4>
-                <h5>Desenvolvedor</h5>
+                <h4>{nome}</h4>
+                <h5>{cargo}</h5>
             </div>
         </div>
     )
